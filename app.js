ccdad9d5719002f4c03a46faa2190268c6c55d64
tyulyukov <makstyulyukov@gmail.com>
2022-05-28 17:15:06 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let multer = require('multer');
+let mongoose = require("mongoose");
+
+let indexRouter = require('./routes/index');
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
